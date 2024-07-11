943edb1e1fecb5329a4f5eac81a36f478ee21d7f
Rakesh <krakesh7788@gmail.com>
2024-07-11 12:45:53 +0530

    added playwright libraray for scraping

--- scrappers/internshalaScrapper/fetchData.js
+++ scrappers/internshalaScrapper/fetchData.js
@@ -1,13 +1,12 @@
-import puppeteer from "puppeteer"
+import { chromium } from "playwright"
+import { MongoClient } from "mongodb"
 import Internshala from "../../models/internshala.js"
 
 export async function fetchInternshalaData() {
   try {
-    const browser = await puppeteer.launch({
-      headless: false,
+    const browser = await chromium.launch({
+      headless: false, // Change to true to run in headless mode
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      timeout: 60000,
-      dumpio: true, // Log browser process stdout and stderr
     })
     const page = await browser.newPage()
     await page.goto(
@@ -16,26 +15,24 @@ export async function fetchInternshalaData() {
 
     const jobs = await page.$$eval(
       "#internship_list_container_1 .individual_internship",
-      (ele) =>
-        ele.map((e) => ({
+      (elements) =>
+        elements.map((e) => ({
           title: e.querySelector(".job-internship-name")?.innerText || "",
           companyName: e.querySelector(".company-name")?.innerText || "",
           location: e.querySelector(".locations")?.innerText || "",
           stipend: e.querySelector(".stipend")?.innerText || "",
           jobType: e.querySelector(".status-li")?.innerText || "",
           url: `https://internshala.com${e.getAttribute("data-href")}` || "",
-          img: e.querySelector(".internship_logo img")?.src ?? "",
+          img: e.querySelector(".internship_logo img")?.src || "",
         }))
     )
-    // console.log(jobs)
-    await Internshala.deleteMany({ jobs })
 
-    // Save new data to MongoDB
-    await Internshala.create(jobs)
+    // Clear existing jobs and insert new ones
+    await Internshala.deleteMany({})
+    await Internshala.insertMany(jobs)
 
     console.log("Internshala Data fetched and saved successfully.")
     await browser.close()
-    return jobs
   } catch (error) {
     console.error("Error fetching or saving data:", error)
   }
